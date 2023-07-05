a2382bba571e173c92ee6dc08679438abf53d997
pbemory-job <pbemory@users.noreply.github.com>
2023-07-04 22:59:33 -0500

    Pass new word args to word bank.

--- source/app.tsx
+++ source/app.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Text } from 'ink';
+import { appendFileSync } from 'fs';
 
 type Props = {
   add?: string[] | undefined;
 };
 
 export default function App({ add }: Props) {
+  {add?.map((word) => {
+    const row = `${word},0\n`
+    try {
+      //fs is considering pwd as vocab_ink/
+      appendFileSync('./word_bank.csv', row);
+    } catch (err) {
+      console.error(err);
+    }
+  })}
   return (
     <>
     {add?.map((word) => (
-      <Text>
-        Hello, <Text color="green">{word}</Text>
+      <Text key={word}>
+        Helloz, <Text color="green">{word}</Text>
       </Text>
     ))}
     </>
