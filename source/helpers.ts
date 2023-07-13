c188e678af26478d368d2f1ad72c477f92932001
pbemory-job <pbemory@users.noreply.github.com>
2023-07-12 21:42:30 -0500

    Continue with readHistory()

--- source/helpers.ts
+++ source/helpers.ts
@@ -1,27 +1,46 @@
 import { appendFileSync, createReadStream } from 'fs';
-import {parse} from 'fast-csv';
-import path, {dirname} from 'path';
+import { parse } from 'fast-csv';
+import path, { dirname } from 'path';
 
 export function launch() {
   readHistory();
 }
 
-export function readHistory() {
-  let rows: string[] = [];
-  const mostRecentSunday = getMostRecentSunday();
-  createReadStream(path.resolve(__dirname,'status_db.csv'))
-  .pipe(parse({headers: false}))
-  .on('error', error => console.error(error))
-  .on('data', row => {
-      console.log(row);
-      //each row can be written to db
-      rows.push(row);
-  })
-  .on('end', (rowCount: any) => {
-      console.log(`Parsed ${rowCount} rows`);
-      //console.log(rows[81484]?.postcode); // this data can be used to write to a db in bulk
-  });
+export type ReadHistoryData = {
+  wordsLearnedThisWeek: number,
+  wordsRemaining: number,
+}
 
+export function readHistory() {
+  const mostRecentSunday = getMostRecentSunday();
+  let rowCounter = 0;
+  let checkedThisWeek = true;
+  let wordsRemaining = 0
+  createReadStream('./utils/status_db.csv')
+    .pipe(parse({ headers: false }))
+    .on('error', error => console.error(error))
+    .on('data', row => {
+      if (rowCounter == 0) {
+        if(new Date(mostRecentSunday) > new Date(row[1])) {
+          const wordsLearnedThisWeek = 0
+          checkedThisWeek = false;
+        }
+      } else if (rowCounter == 1 && checkedThisWeek) {
+        const wordsLearnedThisWeek = Number(row[1])
+      } else if (rowCounter == 2) {
+        wordsRemaining = Number(row[1])
+      }
+      rowCounter++;
+    })
+    .on('end', (rowCount: any) => {
+      //console.log(`Parsed ${rowCount} rows`);
+      //console.log(rows[81484]?.postcode);
+    });
+    const readHistoryData: ReadHistoryData = {
+      wordsLearnedThisWeek: wordsRemaining,
+      wordsRemaining: wordsRemaining
+    }
+    return readHistoryData;
 }
 
 export function getMostRecentSunday() {
@@ -31,11 +50,11 @@ export function getMostRecentSunday() {
   return most_recent_sunday;
 }
 
-export function addWordToWordBank(word:string) {
+export function addWordToWordBank(word: string) {
   const row = `${word},0\n`
   try {
     //fs considers vocab_ink/ the current dir
-    appendFileSync('../utils/word_bank.csv', row);
+    appendFileSync('./utils/word_bank.csv', row);
   } catch (err) {
     console.error(err);
   }
