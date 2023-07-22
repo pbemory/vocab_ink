a03c06a9bfbc1994bb25c2bea2cdcc35884e40c1
pbemory-job <pbemory@users.noreply.github.com>
2023-07-22 18:55:18 -0500

    Small design tweaks.

--- source/components/HistoryDataDisplay.tsx
+++ source/components/HistoryDataDisplay.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { ReadHistoryData } from "../helpers.js";
-import { Text, Box } from 'ink';
+import { Text, Box, Newline } from 'ink';
 
 export default function HistoryDataDisplay({ wordsLearnedThisWeek, wordsRemaining }: ReadHistoryData) {
   return (
     <>
-    <Box borderStyle="classic" width="50%" justifyContent="center">
-    <Text bold color="green">YOUR PROGRESS:</Text>
-    </Box>
-      <Box borderStyle="classic" width="50%" justifyContent="center">
-        <Text>{wordsLearnedThisWeek} words learned this week of {wordsRemaining} words remaining...</Text>
+      <Box borderStyle="round" borderColor="green" width="50%" justifyContent="center">
+        <Text bold color="greenBright">PROGRESS REPORT:</Text>
       </Box>
+      <Box borderStyle="round" borderColor="green" width="50%" justifyContent="center">
+        <Text bold color="green">{wordsLearnedThisWeek}</Text>
+        <Text> words learned this week of </Text><Text color="whiteBright" bold>{wordsRemaining}</Text><Text> words remaining...</Text>
+      </Box>
+
     </>
   )
 }
