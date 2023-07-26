871f6437b0286c07eee92256dec647d21482b1cf
pbemory-job <pbemory@users.noreply.github.com>
2023-07-26 18:03:23 -0500

    Add Axios types

--- source/wordClient.ts
+++ source/wordClient.ts
@@ -10,23 +10,18 @@ const wordsApiBaseUrl = "https://wordsapiv1.p.rapidapi.com/words/"
 const wordnikApiBaseUrl = "https://api.wordnik.com/v4/word.json/"
 const wordnikParams = { 'api_key': wordnikApiKey, 'limit': '3' }
 
-export type WordResult = {
-  definition: string,
-  example: string
-}
-
 function buildAxiosInstances(word: string) {
 
   let instances = [];
   const wordsDefUrl = wordsApiBaseUrl + `${word}/definitions`;
   const wordnikExampleUrl = wordnikApiBaseUrl + `${word}/examples`;
 
-  const wordsInstance = axios.create({
+  const wordsInstance:AxiosInstance = axios.create({
     baseURL: wordsDefUrl,
     headers: wordsApiHeaders
   });
 
-  const wordnikInstance = axios.create({
+  const wordnikInstance:AxiosInstance = axios.create({
     baseURL: wordnikExampleUrl,
     headers: wordnikParams
   });
@@ -37,62 +32,66 @@ function buildAxiosInstances(word: string) {
   return instances;
 }
 
-async function fetchDefinitionAndExample(instances: any[]) {
+export type WordResult = {
+  definition: string,
+  example: string
+}
 
-  return new Promise((resolve,reject)=> {
+async function fetchDefinitionAndExample(instances: AxiosInstance[]) {
 
-  let wordResult: WordResult = {
-    definition: '',
-    example: ''
-  }
+  return new Promise((resolve, reject) => {
 
-  axios.all(instances.map((instance: AxiosInstance) => instance.get(instance.defaults.baseURL!)))
-    .then((data) => {
-      data.forEach(response => {
-        const host = response.request.host;
-        if (String(host).includes('wordnik')) {
-          let wordExample = "No example found.";
-          const exampleKey = 'text'
-          const errorKey = 'message'
-          if (errorKey in response.data) {
-            wordExample = "Error: " + response.data[errorKey];
-          }
-          else {
-            response.data['examples'].every(
-              (element: any) => {
-                if (exampleKey in element) {
-                  wordExample = element[exampleKey];
-                  return false;
-                }
-                return true;
-              }
-            )
-          }
-          wordResult.example = wordExample;
-        } else {
-          let wordDef = "No definition found."
-          const errorKey = 'message';
-          if (errorKey in response.data) {
-            wordDef = "Error: " + response.data[errorKey]
-          }
-          else {
-            const definitions = response.data.definitions;
-            if (definitions.length > 0) {
-              wordDef = definitions[0]['definition']
+    let wordResult: WordResult = {
+      definition: '',
+      example: ''
+    }
+
+    axios.all(instances.map((instance: AxiosInstance) => instance.get(instance.defaults.baseURL!)))
+      .then((data) => {
+        data.forEach(response => {
+          const host = response.request.host;
+          if (String(host).includes('wordnik')) {
+            let wordExample = "No example found.";
+            const exampleKey = 'text'
+            const errorKey = 'message'
+            if (errorKey in response.data) {
+              wordExample = "Error: " + response.data[errorKey];
             }
+            else {
+              response.data['examples'].every(
+                (element: any) => {
+                  if (exampleKey in element) {
+                    wordExample = element[exampleKey];
+                    return false;
+                  }
+                  return true;
+                }
+              )
+            }
+            wordResult.example = wordExample;
+          } else {
+            let wordDef = "No definition found."
+            const errorKey = 'message';
+            if (errorKey in response.data) {
+              wordDef = "Error: " + response.data[errorKey]
+            }
+            else {
+              const definitions = response.data.definitions;
+              if (definitions.length > 0) {
+                wordDef = definitions[0]['definition']
+              }
+            }
+            wordResult.definition = wordDef;
           }
-          wordResult.definition = wordDef;
-        }
+        })
+        return resolve(wordResult);
       })
-      return resolve(wordResult);
-    })
-    .catch(error => reject(error))
-
+      .catch(error => reject(error))
   });
 }
 
 async function testResults() {
-  const testInstances = buildAxiosInstances('lovely');
+  const testInstances: AxiosInstance[] = buildAxiosInstances('lovely');
   const testResults = await fetchDefinitionAndExample(testInstances);
   console.log(testResults);
 }
