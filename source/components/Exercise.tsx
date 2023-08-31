6196af62e5bd2b4d4cee95ae420d985dc272b64e
pbemory-job <pbemory@users.noreply.github.com>
2023-08-31 15:51:45 -0500

    Refactor: move Exercise logic to helpers.

--- source/components/Exercise.tsx
+++ source/components/Exercise.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { createReadStream, createWriteStream } from 'fs';
 import { stringify } from 'csv-stringify';
 import { Text, Box, useApp } from 'ink';
-import { shuffleArray, readRows, ReadHistoryData, getMostRecentSunday } from '../helpers.js';
+import { shuffleArray, readRows, ReadHistoryData, getMostRecentSunday, save, updateWordBankFromScore } from '../helpers.js';
 import { WordResult, buildAxiosInstances, fetchDefinitionAndExample, parseDefinitionAndExample } from '../wordClient.js';
 import Question from './Question.js';
 import { AxiosInstance } from 'axios';
@@ -26,16 +26,13 @@ export default function Exercise({ wordsLearnedThisWeek, wordsRemaining }: ReadH
   const [score, setScore] = useState<number[]>([]);
   const [calledFromExample, setCalledFromExample] = useState(false);
   const [userSaved, setUserSaved] = useState(false);
-
   const { exit } = useApp();
 
   useEffect(() => {
 
     const getWordBank = async () => {
-      const filePath = path.join(__dirname, '../../utils/word_bank.csv');
-      const readable = createReadStream(filePath, { encoding: 'utf8' });
-      let wordBankRows: any[] = await readRows(readable);
-      readable.close();
+      
+      let wordBankRows: any[] = await readRows('../utils/word_bank.csv');
       //shuffle it
       oldWordBankRef.current = shuffleArray(wordBankRows);
       let localWord: any;
@@ -44,20 +41,16 @@ export default function Exercise({ wordsLearnedThisWeek, wordsRemaining }: ReadH
         if (wordBankRow[1].trim() == '1') {
           newWordBankRef.current = [...newWordBankRef.current, wordBankRow];
           rowIndex++;
-          //console.log(wordBankRow[0] + " has a 1.")
           return true;
         }
         else {
           localWord = oldWordBankRef.current[rowIndex][0];
           setCurrentWord(localWord);
           setRowCursor(rowIndex);
-          //console.log(wordBankRow[0] + " has a 0.")
-          //console.log("We should be at element " + rowIndex);
           return false;
         }
       }
       oldWordBankRef.current.every(findNewWord);
-
       //get the word from the word bank + fetch def/example
       const wordInstances: AxiosInstance[] = buildAxiosInstances(localWord);
       const rawWordResults = await fetchDefinitionAndExample(wordInstances);
@@ -70,32 +63,12 @@ export default function Exercise({ wordsLearnedThisWeek, wordsRemaining }: ReadH
   }, [])
 
   useEffect(() => {
-    //skip on first render
     if (calledFromExample) {
       setCalledFromExample(false);
-      //console.log("What is my score? " + score);
-      //console.log("Did I get my last question right? " + score[score?.length - 1])
-      if (score[score?.length - 1] === 0) {
-        //console.log("Getting into truthiness");
-        //console.log("This is the row I'm about to mark with 0 " + oldWordBankRef.current[rowCursor - 1])
-        const copyWordRowToUpdate = [...oldWordBankRef.current[rowCursor - 1]]
-        newWordBankRef.current = [...newWordBankRef.current, copyWordRowToUpdate];
-      }
-      else {
-        //console.log("Getting into else.");
-        //console.log("This is the row I'm about to mark with 1 " + oldWordBankRef.current[rowCursor - 1])
-        const copyWordRowToUpdate = [...oldWordBankRef.current[rowCursor - 1]]
-        copyWordRowToUpdate[1] = '1'
-        newWordBankRef.current = [...newWordBankRef.current, copyWordRowToUpdate];
-      }
-      //console.log("Old word bank: " + oldWordBankRef.current.slice(0, rowCursor));
-      //console.log("New word bank: " + newWordBankRef.current);
-
+      updateWordBankFromScore(score,oldWordBankRef,newWordBankRef,rowCursor);
       const updateQuestion = async () => {
-
         let rowIndex = rowCursor;
         let localWord: any;
-
         const findNewWord = (wordBankRow: any) => {
           if (wordBankRow[1].trim() == '1') {
             newWordBankRef.current = [...newWordBankRef.current, wordBankRow];
@@ -103,14 +76,12 @@ export default function Exercise({ wordsLearnedThisWeek, wordsRemaining }: ReadH
             return true;
           }
           else {
-            //console.log("I found a word that is new. Is is: " + wordBankRow[0] + wordBankRow[1])
             localWord = oldWordBankRef.current[rowIndex][0];
             setCurrentWord(localWord);
             setRowCursor(rowIndex);
             return false;
           }
         }
-
         oldWordBankRef.current.slice(rowIndex).every(findNewWord);
 
         const wordInstances: AxiosInstance[] = buildAxiosInstances(localWord);
@@ -125,48 +96,12 @@ export default function Exercise({ wordsLearnedThisWeek, wordsRemaining }: ReadH
 
   useEffect(() => {
     if (userSaved) {
-      const save = async () => {
-        const filePath = path.join(__dirname, '../../utils/word_bank.csv');
-        const dbFilePath = path.join(__dirname, '../../utils/status_db.csv');
-        const writeStream = createWriteStream(filePath, { encoding: 'utf8' });
-        const dbWriteStream = createWriteStream(dbFilePath, { encoding: 'utf8' });
-        const stringifier = stringify({ header: false });
-        const dbStringifier = stringify({ header: false });
-        //newWordBankRef.current.map((row) => stringifier.write(row));
-        //oldWordBankRef.current.slice(rowCursor).map((row) => stringifier.write(row));
-        let wordsRemaining = 0;
-        for await(const row of newWordBankRef.current) {
-          if(row[1].trim() == '0'){
-            wordsRemaining++;
-          }
-          stringifier.write(row)
-        }
-        for await(const row of oldWordBankRef.current.slice(rowCursor)) {
-          if(row[1].trim() == '0'){
-            wordsRemaining++;
-          }
-          stringifier.write(row);
-        }
-        dbStringifier.write(["Date of last checked Sunday", String(getMostRecentSunday())])
-        dbStringifier.write(["Words learned since Sunday", String(wordsLearnedThisWeek + score.filter(i => i == 1).length)])
-        dbStringifier.write(["Words remaining",String(wordsRemaining)])
-
-        stringifier.pipe(writeStream);
-        dbStringifier.pipe(dbWriteStream);
-
-
-
-
-
-      }
-      save()
+      save(oldWordBankRef,newWordBankRef,wordsLearnedThisWeek,rowCursor,score)
         .then(() => {
           exit();
         })
     }
-
   }, [userSaved])
-
 
   return (
     <>
--- source/helpers.ts
+++ source/helpers.ts
@@ -1,5 +1,6 @@
 import { appendFileSync, createReadStream, createWriteStream, ReadStream } from 'fs';
 import { parse, format } from 'fast-csv';
+import { stringify } from 'csv-stringify';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -15,10 +16,7 @@ export type ReadHistoryData = {
 }
 
 export async function getReadHistory() {
-  const filePath = path.join(__dirname, '../utils/status_db.csv');
-  const readable = createReadStream(filePath, { encoding: 'utf8' });
-  const rawHistoryData = await readRows(readable);
-  readable.close();
+  const rawHistoryData = await readRows('../utils/status_db.csv');
   const mostRecentSunday = getMostRecentSunday();
   let rowCounter = 0;
   let checkedThisWeek = true;
@@ -41,11 +39,14 @@ export async function getReadHistory() {
   return readHistoryData;
 }
 
-export async function readRows(readable: ReadStream) {
+export async function readRows(filepath: string) {
+  const filePath = path.join(__dirname, filepath);
+  const readable = createReadStream(filePath, { encoding: 'utf8' });
   let data: any[] = [];
   for await (const row of readable.pipe(parse({ headers: false }))) {
     data.push(row);
   }
+  readable.close()
   return data;
 }
 
@@ -66,11 +67,52 @@ export function addNewWordToWordBank(word: string) {
   }
 }
 
+export function updateWordBankFromScore(score: number[], oldWordBankRef: any, newWordBankRef: any, rowCursor: any) {
+  if (score[score?.length - 1] === 0) {
+    const copyWordRowToUpdate = [...oldWordBankRef.current[rowCursor - 1]]
+    newWordBankRef.current = [...newWordBankRef.current, copyWordRowToUpdate];
+  }
+  else {
+    const copyWordRowToUpdate = [...oldWordBankRef.current[rowCursor - 1]]
+    copyWordRowToUpdate[1] = '1'
+    newWordBankRef.current = [...newWordBankRef.current, copyWordRowToUpdate];
+  }
+}
+
+export async function save(oldWordBankRef: any, newWordBankRef: any, wordsLearnedThisWeek: any, rowCursor: any, score: any) {
+  const filePath = path.join(__dirname, '../utils/word_bank.csv');
+  const dbFilePath = path.join(__dirname, '../utils/status_db.csv');
+  const writeStream = createWriteStream(filePath, { encoding: 'utf8' });
+  const dbWriteStream = createWriteStream(dbFilePath, { encoding: 'utf8' });
+  const stringifier = stringify({ header: false });
+  const dbStringifier = stringify({ header: false });
+  let wordsRemaining = 0;
+  for await (const row of newWordBankRef.current) {
+    if (row[1].trim() == '0') {
+      wordsRemaining++;
+    }
+    stringifier.write(row)
+  }
+  for await (const row of oldWordBankRef.current.slice(rowCursor)) {
+    if (row[1].trim() == '0') {
+      wordsRemaining++;
+    }
+    stringifier.write(row);
+  }
+  dbStringifier.write(["Date of last checked Sunday", String(getMostRecentSunday())])
+  dbStringifier.write(["Words learned since Sunday", String(wordsLearnedThisWeek + score.filter((i: any) => i == 1).length)])
+  dbStringifier.write(["Words remaining", String(wordsRemaining)])
+
+  stringifier.pipe(writeStream);
+  dbStringifier.pipe(dbWriteStream);
+
+}
+
 //credit: Durstenfeldt shuffle
-export function shuffleArray(array:any[]) {
+export function shuffleArray(array: any[]) {
   for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 }
